72d3263c7e1af8423eac57c24d1e553bbca7b219
Arinze <arinzeemekpo@gmail.com>
2022-06-23 17:20:44 +0100

    initial project setup

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react'
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { StyleSheet, View, SafeAreaView, StatusBar, Platform, Text, ScrollView } from 'react-native';
 
 export default function App() {
+  const [todos,setTodos] = useState([
+    {item:'Play football',Key:'1'},
+    {item:'Read Textbook',Key:'2'},
+    {item:'Cook For Family',Key:'3'},
+    {item:'Do React Native Project',Key:'4'},
+    {item:'Learn To Play Guitar',Key:'5'},
+    {item:'Call dad and mom',Key:'6'},
+    {item:'Transfer to Electrician',Key:'7'},
+    {item:'Transfer to mom',Key:'8'},
+    {item:'Transfer to dad',Key:'9'},
+  ]);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView>
+        <View style={styles.AD}>
+          <ScrollView>
+          {
+            todos.map((todo) => {
+              return <Text style={styles.todotext}>{todo.item}</Text>
+            })
+          }
+          </ScrollView>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  AD: {
+    marginTop: Platform.OS == 'android' ? StatusBar.currentHeight : 0
   },
+  todotext:{
+    backgroundColor: 'skyblue',
+    paddingVertical: 24,
+    paddingHorizontal: 10,
+    marginVertical: 10,
+    marginHorizontal: 10,
+    borderWidth: 1,
+    borderColor: 'blue',
+    borderRadius: 10
+  }
 });
